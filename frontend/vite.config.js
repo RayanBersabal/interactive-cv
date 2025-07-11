e5c9f4e5ab47cb8d9edb5aec3234fd65da24fb73
RayanBersabal <rayanbersabal@gmail.com>
2025-07-11 22:39:47 +0700

    retry vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-//import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    //vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: '../dist', // Output dist at root level (next to vercel.json)
+    emptyOutDir: true, // Clean old build files
+  }
 })
